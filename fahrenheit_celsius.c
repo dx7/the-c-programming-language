5bfd5caa7d1399a07e15aa8f6b2bd3380aa61026
rafaeldx7 <rafaeldx7@gmail.com>
2011-12-29 23:30:21 -0200

    using floating

--- fahrenheit_celsius.c
+++ fahrenheit_celsius.c
@@ -5,8 +5,8 @@
 
 main()
 {
-  int fahr, celsius;
-  int lower, upper, step;
+  float fahr, celsius;
+  float lower, upper, step;
 
   lower = 0;
   upper = 300;
@@ -15,8 +15,8 @@ main()
   fahr = lower;
   while (fahr <= upper)
   {
-    celsius = 5 * (fahr - 32) / 9;
-    printf("%3d %6d\n", fahr, celsius);
+    celsius = (5.0 / 9.0) * (fahr - 32);
+    printf("%3.0f %6.1f\n", fahr, celsius);
     fahr = fahr + step;
   }
 }
